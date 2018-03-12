dcb6f73397b223e35988d13297a293620e4ea868
RandyCoffman <randy.coffman@wvjc.edu>
2018-03-12 12:49:07 -0400

    final tests are passing for the .delete method

--- delete_character_test.rb
+++ delete_character_test.rb
@@ -7,6 +7,22 @@ class Delete_method < Minitest::Test
 	end
 
 	def test_string
-		assert_equal(String, delete.class)
+		assert_equal(String, delete("abc", "a").class)
+	end
+
+	def test_delete
+		assert_equal("heo", delete("hello", "l"))
+	end
+
+	def test_delete2
+		assert_equal("gdbye", delete("goodbye", "o"))
+	end
+
+	def test_delete3
+		assert_equal("the cake s a le", delete("the cake is a lie", "i"))
+	end
+
+	def test_delete4
+		assert_equal("chickens ike cookies", delete("chickens like cookies", "l"))
 	end
 end
